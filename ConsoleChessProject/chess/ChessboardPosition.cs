9d68556429cbf9c2712e16e9464334699a111237
Douglas Reinaldo <Douglas39510@gmail.com>
2025-07-27 17:12:30 -0300

    ChessboardPosition clas created

---
+++ ConsoleChessProject/chess/ChessboardPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ConsoleChessProject.chessboard;
+
+namespace ConsoleChessProject.chess
+{
+    class ChessboardPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessboardPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+
+        public Position toPosition() 
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
